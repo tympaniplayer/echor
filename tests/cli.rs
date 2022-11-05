14d0beec60d6180d84bb26e221e0accaf37893c5
Nate Palmer <nate@natepalmer.dev>
2022-11-04 21:11:19 -0400

    Refactor tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -5,57 +5,66 @@ use std::fs;
 type TestResult = Result<(), Box<dyn std::error::Error>>;
 
 #[test]
-fn dies_no_args() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn dies_no_args() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.assert()
         .failure()
         .stderr(predicate::str::contains("Usage"));
+    Ok(())
 }
 
 #[test]
-fn runs() {
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn runs() -> TestResult {
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("hello").assert().success();
+    Ok(())
 }
 
 #[test]
-fn hello1() {
-    let expected = get_expected_from_file("tests/expected/hello1.txt");
-    let mut cmd = Command::cargo_bin("echor").unwrap();
+fn hello1() -> TestResult {
+    let expected = get_expected_from_file("tests/expected/hello1.txt")?;
+    let mut cmd = Command::cargo_bin("echor")?;
     cmd.arg("Hello there").assert().success().stdout(expected);
+    Ok(())
 }
 
 #[test]
-fn hello2() {
-    let expected = get_expected_from_file("tests/expected/hello2.txt");
-    let mut cmd = get_echor_command();
+fn hello2() -> TestResult {
+    let expected = get_expected_from_file("tests/expected/hello2.txt")?;
+    let mut cmd = get_echor_command()?;
     let args = vec!["Hello", "there"];
     cmd.args(args).assert().success().stdout(expected);
+    Ok(())
 }
 
 #[test]
-fn hello1_n() {
-    let expected = get_expected_from_file("tests/expected/hello1.n.txt");
-    let mut cmd = get_echor_command();
+fn hello1_n() -> TestResult {
+    let expected = get_expected_from_file("tests/expected/hello1.n.txt")?;
+    let mut cmd = get_echor_command()?;
     cmd.arg("-n")
         .arg("Hello  there")
         .assert()
         .success()
         .stdout(expected);
+    Ok(())
 }
 
 #[test]
-fn hello2_n() {
-    let expected = get_expected_from_file("tests/expected/hello2.n.txt");
-    let mut cmd = get_echor_command();
+fn hello2_n() -> TestResult {
+    let expected = get_expected_from_file("tests/expected/hello2.n.txt")?;
+    let mut cmd = get_echor_command()?;
     let args = vec!["-n", "Hello", "there"];
     cmd.args(args).assert().success().stdout(expected);
+    
+    Ok(())
 }
 
-fn get_echor_command() -> Command {
-    Command::cargo_bin("echor").unwrap()
+fn get_echor_command() -> Result<Command, Box<dyn std::error::Error>> {
+    let command = Command::cargo_bin("echor")?;
+    Ok(command)
 }
 
-fn get_expected_from_file(outfile: &str) -> String {
-    fs::read_to_string(outfile).unwrap()
+fn get_expected_from_file(outfile: &str) -> Result<String, Box<dyn std::error::Error>> {
+    let file = fs::read_to_string(outfile)?;
+    Ok(file)
 }
